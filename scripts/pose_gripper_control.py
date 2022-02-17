4219bbc16226be0ef2e58e2aa3c8572efcfed93d
Yunfei Li <yunfeili.cloud@gmail.com>
2022-02-17 18:34:08 +0800

    use timer to post target periodically

--- scripts/pose_gripper_control.py
+++ scripts/pose_gripper_control.py
@@ -5,35 +5,47 @@ import sys
 import rospy
 import std_msgs.msg
 import geometry_msgs.msg
+import copy
 
 
 def main():
     fake = sys.argv[1] == "true"
     ns = sys.argv[2]
-    ready_pub = rospy.Publisher(ns + "/ready", std_msgs.msg.Bool, 1)
+    ready_pub = rospy.Publisher("ready", std_msgs.msg.Bool, queue_size=1)
     client = MoveGroupPythonIntefaceTutorial(fake)
     arm_ready = True
     gripper_ready = True
+    initial_pose = client.get_pose()
 
     def arm_callback(data):
         print ns + " arm got target"
+        print data
+        # for debugging
+        # data = copy.deepcopy(initial_pose)
+        # data.position.z += 0.1
         client.go_to_pose_goal(data)
+        global arm_ready
         arm_ready = True
     
     def gripper_callback(data):
         print(ns + " gripper got target...")
         client.move_gripper_to(data)
+        global gripper_ready
         gripper_ready = True
 
-    rospy.Subsriber(ns + "/arm_target_pose", geometry_msgs.msg.Pose, arm_callback)
-    rospy.Subscriber(ns + "/gripper_target", std_msgs.msg.Float64, gripper_callback)
+    rospy.Subscriber("arm_target_pose", geometry_msgs.msg.Pose, arm_callback)
+    rospy.Subscriber("gripper_target", std_msgs.msg.Float64, gripper_callback)
 
     while not rospy.is_shutdown():
         if arm_ready and gripper_ready:
-            ready_pub.publish(1)
+            print "========publish ready message"
+            msg = std_msgs.msg.Bool()
+            msg.data = True
+            ready_pub.publish(msg)
+            # Comment only for debugging
             arm_ready = False
             gripper_ready = False
-        rospy.sleep(0.01)
+        # rospy.sleep(0.01)
 
 
 
--- scripts/sync_node.py
+++ scripts/sync_node.py
@@ -9,57 +9,90 @@ import math
 # if all ready flags are true, pub a message to move to the next step, clear ready flags
 
 # in each control node, pub ready flag before the first move; subscribe to ``move to next'' message, and call execution in its callback function
-    
+
+step = 0
 def main():
     ready_flags = [False, False]
     file_name = sys.argv[1]
+    print "========== going to load " + file_name
     with open(file_name, "rb") as f:
         data = pickle.load(f)
     panda0_traj_ee = data["panda0_ee"]
     panda0_traj_gripper = data["panda0_finger"]
     panda1_traj_ee = data["panda1_ee"]
     panda1_traj_gripper = data["panda1_finger"]
+    print "=========Data loaded"
     assert len(panda0_traj_ee) == len(panda1_traj_ee) and len(panda0_traj_ee) == len(panda0_traj_gripper) and len(panda0_traj_gripper) == len(panda1_traj_gripper)
     arm0_pose_pub = rospy.Publisher('/robot1/arm_target_pose', geometry_msgs.msg.Pose, queue_size=1)
     gripper0_pub = rospy.Publisher('/robot1/gripper_target', std_msgs.msg.Float64, queue_size=1)
     arm1_pose_pub = rospy.Publisher('/robot2/arm_target_pose', geometry_msgs.msg.Pose, queue_size=1)
     gripper1_pub = rospy.Publisher('/robot2/gripper_target', std_msgs.msg.Float64, queue_size=1)
+    rospy.init_node("synchronizer", anonymous=True)
     
     def callback0(data):
-        print "robot1 ready"
-        ready_flags[0] = data
+        ready_flags[0] = data.data
+        print "robot1 ready " + str(ready_flags[0])
 
     def callback1(data):
-        print "robot2 ready"
-        ready_flags[1] = data
+        ready_flags[1] = data.data
+        print "robot2 ready " + str(ready_flags[1])
     
-    rospy.Subscriber('/robot1/ready', std_msgs.msg.Bool, callback0)
-    rospy.Subscriber('/robot2/ready', std_msgs.msg.Bool, callback1)
+    # rospy.Subscriber('/robot1/ready', std_msgs.msg.Bool, callback0)
+    # rospy.Subscriber('/robot2/ready', std_msgs.msg.Bool, callback1)
 
-    step = 0
-    while not rospy.is_shutdown() and step < len(panda0_traj_ee):
-        if ready_flags[0] and ready_flags[1]:
-            # command the next move
-            pose0 = geometry_msgs.msg.Pose()
-            pose0.position.x = panda0_traj_ee[step][0]
-            pose0.position.y = panda0_traj_ee[step][1]
-            pose0.position.z = panda0_traj_ee[step][2]
-            pose0.orientation.x = math.sqrt(2) / 2.0
-            pose0.orientation.y = math.sqrt(2) / 2.0
-            pose1 = geometry_msgs.msg.Pose()
-            pose1.position.x = panda1_traj_ee[step][0]
-            pose1.position.y = panda1_traj_ee[step][1]
-            pose1.position.z = panda1_traj_ee[step][2]
-            pose1.orientation.x = math.sqrt(2) / 2.0
-            pose1.orientation.y = math.sqrt(2) / 2.0
-            arm0_pose_pub.publish(pose0)    
-            arm1_pose_pub.publish(pose1)
-            gripper0_pub.publish(panda0_traj_gripper[step])
-            gripper1_pub.publish(panda1_traj_gripper[step])
+    # step = 0
+    # while not rospy.is_shutdown() and step < len(panda0_traj_ee):
+    #     if ready_flags[0] and ready_flags[1]:
+    #         # command the next move
+    #         pose0 = geometry_msgs.msg.Pose()
+    #         pose0.position.x = panda0_traj_ee[step][0]
+    #         pose0.position.y = panda0_traj_ee[step][1]
+    #         pose0.position.z = panda0_traj_ee[step][2]
+    #         pose0.orientation.x = math.sqrt(2) / 2.0
+    #         pose0.orientation.y = math.sqrt(2) / 2.0
+    #         pose1 = geometry_msgs.msg.Pose()
+    #         pose1.position.x = panda1_traj_ee[step][0]
+    #         pose1.position.y = panda1_traj_ee[step][1]
+    #         pose1.position.z = panda1_traj_ee[step][2]
+    #         pose1.orientation.x = math.sqrt(2) / 2.0
+    #         pose1.orientation.y = math.sqrt(2) / 2.0
+    #         print "========publish target"
+    #         arm0_pose_pub.publish(pose0)    
+    #         arm1_pose_pub.publish(pose1)
+    #         gripper0_pub.publish(panda0_traj_gripper[step])
+    #         gripper1_pub.publish(panda1_traj_gripper[step])
 
-            ready_flags = [False, False]
-            step += 1
+    #         ready_flags = [False, False]
+    #         step += 1
+    #     # rospy.sleep(0.1)
 
+    def callback(event):
+        global step
+        print event.current_expected
+        pose0 = geometry_msgs.msg.Pose()
+        pose0.position.x = panda0_traj_ee[step][0]
+        pose0.position.y = panda0_traj_ee[step][1]
+        pose0.position.z = panda0_traj_ee[step][2]
+        pose0.orientation.x = math.sqrt(2) / 2.0
+        pose0.orientation.y = math.sqrt(2) / 2.0
+        pose1 = geometry_msgs.msg.Pose()
+        pose1.position.x = panda1_traj_ee[step][0]
+        pose1.position.y = panda1_traj_ee[step][1]
+        pose1.position.z = panda1_traj_ee[step][2]
+        pose1.orientation.x = math.sqrt(2) / 2.0
+        pose1.orientation.y = math.sqrt(2) / 2.0
+        print "========publish target"
+        arm0_pose_pub.publish(pose0)    
+        arm1_pose_pub.publish(pose1)
+        gripper0_pub.publish(panda0_traj_gripper[step])
+        gripper1_pub.publish(panda1_traj_gripper[step])
+        step = step + 1
+
+    rospy.sleep(5)
+    print "======Press Enter to start timer"
+    raw_input()
+    timer = rospy.Timer(rospy.Duration(1.0 / 12), callback)
+    timer.run()
 
 if __name__ == "__main__":
     main()
